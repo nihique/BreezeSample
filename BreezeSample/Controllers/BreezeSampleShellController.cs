5568f69649dd5b5451686f0fe24f8c82a495cb7d
nihique <kovarik.martin@gmail.com>
2013-03-28 14:24:16 +0100

    breeze sample nuget

---
+++ BreezeSample/Controllers/BreezeSampleShellController.cs
@@ -0,0 +1,12 @@
+using System.Web.Mvc;
+
+namespace BreezeSample.Controllers
+{
+    public class BreezeSampleShellController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
